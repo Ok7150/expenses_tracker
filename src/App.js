4d16b66e8ddde436b3b21adafa3c1d20e347adfc
Ok7150 <onkarkale7150@gmail.com>
2024-01-17 18:56:32 +0530

    Chart created

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from "react";
-
+import React, {useState} from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 100,
-      date: new Date(2020, 7, 14),
-    },
-    { 
-      id: "e2", 
-      title: "New TV", 
-      amount: 7999, 
-      date: new Date(2021, 2, 12) 
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 9467,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 1450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const dummy_expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 100,
+    date: new Date(2020, 7, 14),
+  },
+  { 
+    id: "e2", 
+    title: "New TV", 
+    amount: 7999, 
+    date: new Date(2021, 2, 12) 
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 9467,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 1450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
+const App = () => {
+
+  const [expenses, setExpenses] = useState(dummy_expenses);
+  
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]; // add new expense to the beginning of the array
+    });
   };
 
   // return React.createElement(
